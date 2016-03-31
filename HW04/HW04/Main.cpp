34980bfdaefecec0b3c2258734f8b25b96b03760
jabbott13153 <julia.abbott97@gmail.com>
2016-03-31 10:34:13 -0700

    Adding HW04 to repository
    finished ex01

---
+++ HW04/HW04/Main.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+
+int* ex01_part_1(int Size);
+int ex01_part2(int*, int);
+
+int main()
+{
+
+	//create variable so user can input an integer into the ex01_part_1 function
+	int size_of_array = 0;
+	
+//prompt user to enter a number, then input number into function
+	cout << "Please enter the size of the new array: \n";
+	cin >> size_of_array;
+
+	//int* array = ex01_part_1(size_of_array);
+	cout << "The amount of numbers above the average is: " << ex01_part2(ex01_part_1(size_of_array), size_of_array) << endl;
+	return 0;
+}
+
+//code to create array using a number inputted into funciton,
+//then reads the requisite amount of numbers into the array
+int* ex01_part_1(int Size)
+{
+	//create variable for pointer to point to
+	int Number;
+
+	//create pointer to be able to input numbers into the array.
+	//Pointer set to NULL
+	int * number = &Number;
+
+	//pointer to allocate memory for new array
+	int* newArray = new int[Size];
+
+	//initialize the array
+	for (int i = 0; i < Size; i++)
+	{
+		
+		cout << "Please enter an integer: " << endl;
+		cin >> *number;
+		newArray[i] = *number;
+	}
+
+	//check to make sure program is working correctly
+	/*for (int i = 0; i < Size; i++)
+	{
+		cout << newArray[i] << endl;
+
+	}*/
+	return newArray;
+}
+
+//function to take average, and then return the numbers above the average
+int ex01_part2(int* new_array, int size)
+{
+	int average = 0;
+	//creates variable to store amount of numbers above the average
+	int tally = 0;
+//finds average
+	for (int i = 0; i < size; i++)
+	{
+		average += new_array[i];
+	}
+	average /= size;
+
+	//checks to see what numers are above the average
+	for (int i = 0; i < size; i++)
+	{
+		if (new_array[i] > average)
+			tally++;			
+	}
+
+	return tally;
+}
